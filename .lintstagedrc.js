0fe4da49dc794e0304155a568afad82086e80c33
LightXJ <wxjneu@gmail.com>
2022-03-15 19:55:05 +0800

    chore: 提交校验fix

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ["eslint --fix --max-warnings 10"],
-  '*.{ts,tsx}': [() => "tsc --skipLibCheck --noEmit"]
+  '*.{js,jsx,ts,tsx}': ["eslint --max-warnings 10"],
+  '*.{ts,tsx}': "tsc-files -p tsconfig.json --noEmit --skipLibCheck typed-css.d.ts next-env.d.ts"
 }
